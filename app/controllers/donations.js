03211ea535080e7bfc16f94cc82fb80b8865fdea
Israel Finnerty <50764018+IsraelFinnerty@users.noreply.github.com>
2020-01-29 21:43:20 +0000

    V2

---
+++ app/controllers/donations.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const Donations = {
+    home: {
+        handler: function(request, h) {
+            return h.view('home', { title: 'Make a Donation' });
+        }
+    },
+    report: {
+        handler: function(request, h) {
+            return h.view('report', {
+                title: 'Donations to Date',
+                donations: this.donations,
+            });
+        }
+    },
+    donate: {
+        handler: function(request, h) {
+            const data = request.payload;
+            var donorEmail = request.auth.credentials.id;
+            data.donor = this.users[donorEmail];
+            this.donations.push(data);
+            console.log(this.donations);
+            return h.redirect('/report');
+        }
+    }
+};
+
+module.exports = Donations;
